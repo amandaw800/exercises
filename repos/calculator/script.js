d5e398fb12c6783ea95f66cdc1faf5eae6093987
amandaw800 <amanda.v.wei@gmail.com>
2025-04-03 13:33:01 -0400

    functioning calc

--- repos/calculator/script.js
+++ repos/calculator/script.js
@@ -16,11 +16,27 @@ function multiply(num1, num2)
 
 function divide(num1, num2)
 {
-    return num1/num2;
+    if(num2 == 0)
+    {
+        return "INVALID"
+    }
+  
+    return (num1/num2).toFixed(2)
 }
 
 function operate(num1, operator, num2)
 {
+    if(isNaN(parseInt(num1)) || isNaN(parseInt(num2))) // if num1 cannot be parsed
+    {
+        return "INVALID";
+
+    }
+    else {
+        num1 = parseInt(num1)
+        num2 = parseInt(num2)
+    }
+ 
+
     switch(operator)
     {
         case "+":
@@ -33,96 +49,114 @@ function operate(num1, operator, num2)
             return divide(num1, num2)
 
         default:
-            return null
+            return "INVALID"
     }
     
 
 }
 
-function parseEquation(equation)
+function convertString(equation)
 {
-    let ans = [];
-    let current = ""
-    let numberPattern = /^[+-]?(\d+(\.\d*)?|\.\d+)$/;
-    let operators = /[+\-*/=]|clear/;
+    if(equation.length < 3)
+    {
+        buttonValues = "INVALID"
+    }
+
+    const parsed = []
+    const operators = ["+", "-", "/", "*"]
+    let operator = ""
 
     for(let i =0; i < equation.length; i++)
     {
-        if(numberPattern.test(equation[i]))
+        if(operators.includes(equation[i]))
         {
-          
-            current += equation[i]
-        }
-        else if(operators.test(equation[i]))
-        {
-            ans.push(current)
-            ans.push(equation[i])
+            operator = equation[i]
 
-            current = "";
-            
         }
-
     }
 
-    if(current != "")
+    if(operator == "")
     {
-        ans.push(current)
-
+        return "No operator"
 
     }
-    return ans;
 
+    let split = equation.split(operator) // [5, 3]
+
+    if(isNaN(parseInt(split[0])) || isNaN(parseInt(split[1])))
+    {
+        return "INVALID"
+    }
+
+    parsed.push(parseInt(split[0]))
+    parsed.push(operator)
+    parsed.push(parseInt(split[1]))
+
+
+    return parsed;
 
 }
 
 
-function buttonPress()
+function clickButton()
 {
-const btns = document.querySelectorAll("button");
+    const digits = document.querySelectorAll("button")
+    const calcDisplay = document.querySelector(".text")
 
+    let buttonValues = ""
+    let calculation = 0
 
-const text = document.querySelector(".text");
-
-btns.forEach((button)=>
-{
-    button.addEventListener("click", () =>{
-        if(button.id == "one")
+    digits.forEach((button)=>
+    {
+        button.addEventListener("click", ()=>
         {
-            text.textContent += "1";
+            if(button.className == "equals")
+            {
+                console.log("Button values are: " + buttonValues)
+        
+                const converted = convertString(buttonValues)
+
+                
+
+                if(calculation == 0)
+                {
+                    calculation = operate(converted[0], converted[1], converted[2])
+                    console.log("First pass is: " + calculation)
+                    
+                }
+                else if (calculation > 0){
+                    
+                    converted[0] = calculation
+                    console.log(converted)
+                    calculation = operate(calculation, converted[1], converted[2])
             
-        }
-        else if(button.id == "two")
-        {
-            text.textContent +="2";
-        }
-        else if(button.id == "+")
-        {
-            text.textContent += "+"
-        }
-        else if(button.id == "=")
-        {
-            let val = text.textContent
-           
-            let parsed = parseEquation(val)
-
-            let calculate = (operate(parseInt(parsed[0]), parsed[1], parseInt(parsed[2])))
+                  
+                    console.log("Second pass is :" + calculation)
+                }
+                buttonValues = calculation
 
 
-
-     
-
-
-
-            console.log(calculate)
-           //compute
-        }
+            }
+            else if(button.className == "number" || button.className == "operator")
+                {
+            
+                    buttonValues += button.textContent
+                    calcDisplay.textContent = buttonValues
     
+                }
+    
+            
+            
+        });
+
     });
 
-});
-
 }
 
-buttonPress()
+
+
+clickButton()
+
+
 
 
